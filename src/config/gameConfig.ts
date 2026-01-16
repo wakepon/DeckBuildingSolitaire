0e4500fedb4fde5ef31ef209213c55baf7f2de37
waken <wakepon@users.noreply.github.com>
2026-01-16 21:29:14 +0900

    gameConfig.ts を更新

--- src/config/gameConfig.ts
+++ src/config/gameConfig.ts
@@ -4,8 +4,8 @@
  */
 
 // プレイヤー設定
-export const PLAYER_INITIAL_HP = 30;
-export const HAND_SIZE = 7;  // 手札の枚数
+export const PLAYER_INITIAL_HP = 15;
+export const HAND_SIZE = 5;  // 手札の枚数
 
 // 場札更新の設定
-export const FIELD_REFRESH_MAX_COUNT = 10;  // 1ステージあたりの場札更新回数上限
+export const FIELD_REFRESH_MAX_COUNT = 4;  // 1ステージあたりの場札更新回数上限
